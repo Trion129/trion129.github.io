efa43071e55f6585f2e173159587bfb454c5b37b
Parminder Singh <parmsingh129@yahoo.com>
2017-06-13 14:56:46 +0530

    Laid out app structure

---
+++ js/main.js
@@ -0,0 +1,4 @@
+require("../css/reset.css")
+require("../css/style.css")
+const p5 = require("p5");
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./js/main.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        { 
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader" 
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              { loader: 'css-loader', options: { importLoaders: 1 } },
+              'postcss-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].css')
+    ]
+};
